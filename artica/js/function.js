256931e0844a4df5d7f5cec3362587401baa8303
ministori <ministori@gmail.com>
2017-07-19 12:54:51 +0900

    add js

---
+++ artica/js/function.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 2017-07-19.
+ */
+
+$(function(){
+
+  // gnb
+  $('.btn-menu-toggle').data({'open' : 'false'}).on('click', function(){
+
+    if( $(this).data('open') == 'false' ){
+
+      $('.gnb').css({right : 0});
+      $(this).data({'open' : 'true'});
+
+    } else {
+
+      $('.gnb').css({right : -340});
+      $(this).data({'open' : 'false'});
+
+    }
+
+  });
+
+  $('.btn-menu-close').on('click', function(){
+    $('.gnb').css({right : -340});
+    $('.btn-menu-toggle').data({'open' : 'false'});
+  });
+
+  $('.gnb-link').on('click', function(e){
+
+    e.preventDefault();
+
+    var href = $(this).attr('href');
+
+    var scrollPosition = $(href).offset().top;
+
+    $('body').stop().animate({
+      scrollTop : scrollPosition
+    });
+
+  });
+
+});
