c24037507db5632f08c163fe0a622bac54632472
ministori <ministori@gmail.com>
2017-07-19 17:27:25 +0900

    add js

--- artica/js/function.js
+++ artica/js/function.js
@@ -41,6 +41,9 @@ $(function(){
   });
 
   // portfolio list
+
+  var prevLineNumber = -1; // 리스트 상세내용이 선택되어 보이지 않는 상태
+
   $('.pofol-list-item>.thumb').on('click', function(){
 
     var indexNumber = $(this).parent().index('.pofol-list-item');
@@ -49,14 +52,63 @@ $(function(){
 
     var detailTop = parseInt( $('.detail-wrap').css('top') ) + ( lineNumber * 230 );
 
-    $(this).next().css({
-      top : detailTop,
-      height : 486
-    });
+    if( prevLineNumber == -1 ){
 
-    for(var k=0; k<=3; k++){
-      $('.pofol-list-item').eq( lineNumber*4 + k ).css({'padding-bottom' : 490});
+      $(this).next().css({
+        top : detailTop,
+        height : 486,
+        transition:'height 1s ease-out'
+      });
+
+      for(var k=0; k<=3; k++){
+        $('.pofol-list-item').eq( lineNumber*4 + k ).css({'padding-bottom' : 490});
+      }
+
+      prevLineNumber = lineNumber;
+
+    } else if( prevLineNumber == lineNumber ){
+
+      $('.detail-wrap').css({
+        height : 0,
+        transition : 'none'
+      });
+
+      $(this).next().css({
+        top : detailTop,
+        height : 486
+      });
+
+      for(var l=0; l<=3; l++){
+        $('.pofol-list-item').eq( lineNumber*4 + l ).css({'padding-bottom' : 490});
+      }
+
+      prevLineNumber = lineNumber;
+
+    } else {
+
+      $('.detail-wrap').css({
+        height : 0,
+        transition : 'height 1s ease-out'
+      });
+
+      for(var j=0; j<=3; j++){
+        $('.pofol-list-item').eq( prevLineNumber*4 + j ).css({'padding-bottom' : 0});
+      }
+
+      $(this).next().css({
+        top : detailTop,
+        height : 486
+      });
+
+      for(var i=0; i<=3; i++){
+        $('.pofol-list-item').eq( lineNumber*4 + i ).css({'padding-bottom' : 490});
+      }
+
+      prevLineNumber = lineNumber;
+
     }
+
+
 
 /*
     if( indexNumber >= 0 && indexNumber <=3 ){
