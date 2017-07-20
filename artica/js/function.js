00552c166e43a1ce3804971097c8464a9f784303
ministori <ministori@gmail.com>
2017-07-20 10:51:32 +0900

    add js

--- artica/js/function.js
+++ artica/js/function.js
@@ -40,101 +40,10 @@ $(function(){
 
   });
 
-  // portfolio list
+  // about us - my story
+  $('.item-photo-mask').on('click', function(){
 
-  var prevLineNumber = -1; // 리스트 상세내용이 선택되어 보이지 않는 상태
-
-  $('.pofol-list-item>.thumb').on('click', function(){
-
-    var indexNumber = $(this).parent().index('.pofol-list-item');
-
-    var lineNumber = Math.floor(indexNumber/4);
-
-    var detailTop = parseInt( $('.detail-wrap').css('top') ) + ( lineNumber * 230 );
-
-    if( prevLineNumber == -1 ){
-
-      $(this).next().css({
-        top : detailTop,
-        height : 486,
-        transition:'height 1s ease-out'
-      });
-
-      for(var k=0; k<=3; k++){
-        $('.pofol-list-item').eq( lineNumber*4 + k ).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    } else if( prevLineNumber == lineNumber ){
-
-      $('.detail-wrap').css({
-        height : 0,
-        transition : 'none'
-      });
-
-      $(this).next().css({
-        top : detailTop,
-        height : 486
-      });
-
-      for(var l=0; l<=3; l++){
-        $('.pofol-list-item').eq( lineNumber*4 + l ).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    } else {
-
-      $('.detail-wrap').css({
-        height : 0,
-        transition : 'height 1s ease-out'
-      });
-
-      for(var j=0; j<=3; j++){
-        $('.pofol-list-item').eq( prevLineNumber*4 + j ).css({'padding-bottom' : 0});
-      }
-
-      $(this).next().css({
-        top : detailTop,
-        height : 486
-      });
-
-      for(var i=0; i<=3; i++){
-        $('.pofol-list-item').eq( lineNumber*4 + i ).css({'padding-bottom' : 490});
-      }
-
-      prevLineNumber = lineNumber;
-
-    }
-
-
-
-/*
-    if( indexNumber >= 0 && indexNumber <=3 ){
-      for(var i=0; i<=3; i++){
-        $('.pofol-list-item').eq(i).css({'padding-bottom' : 490});
-      }
-    } else if( indexNumber >= 4 && indexNumber <= 7 ){
-      for(var j=4; j<=7; j++){
-        $('.pofol-list-item').eq(j).css({'padding-bottom' : 490});
-      }
-    }
-*/
-
-  });
-
-  $('.pofol-list-item .btn-pop-close').on('click', function(){
-
-    var indexNumber = $(this).parents('.pofol-list-item').index('.pofol-list-item');
-
-    var lineNumber = Math.floor(indexNumber/4);
-
-    $('.detail-wrap').css({height : 0});
-
-    for(var k=0; k<=3; k++){
-      $('.pofol-list-item').eq( lineNumber*4 + k ).css({'padding-bottom' : 0});
-    }
+    $(this).parent().next().css({height : 279});
 
   });
 
